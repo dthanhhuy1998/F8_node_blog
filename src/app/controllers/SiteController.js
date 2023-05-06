fa2ce849a8c8b9dec0e529f2a832c1863ae0ddb8
dthanhhuy.me <dthanhhuy.me@gmail.com>
2023-05-06 22:54:19 +0700

    fetch data from mongoose db

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,16 +1,16 @@
 const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
     // [GET] /
-    index(req, res) {
-        try {
-            Course.find({}).then(courses => {
-                res.json(courses);
-            });
-            return;
-        } catch (err) {
-            console.log(err);
-        };
+    index(req, res, next) {
+        Course.find({})
+            .then(courses => {
+                res.render('home', { 
+                    courses: multipleMongooseToObject(courses),
+                });
+            })
+            .catch(next); // catch(err => next(err))
     }
 
     // [GET] /search
